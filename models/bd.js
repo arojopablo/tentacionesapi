a6ca579444dde2154920fd937de7ae6b6846172d
arojopablo <arojopablo@gmail.com>
2023-04-09 22:16:38 -0300

    tiempo de time out

--- models/bd.js
+++ models/bd.js
@@ -3,6 +3,10 @@ var util = require('util');
 
 var pool = mysql.createPool({
     connectionLimit: 10,
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
     host:         process.env.MYSQL_HOST,
     user:        process.env.MYSQL_USER,
     password:    process.env.MYSQL_PASSWORD,
